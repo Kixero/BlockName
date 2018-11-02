881f08b2a889c36543cd433f0b89c1917a0aac28
Kixero <anthony.meranger@gmail.com>
2018-11-02 21:51:36 +0100

    Added documentation.

--- src/main/java/io/github/kixero/blockname/BlockName.java
+++ src/main/java/io/github/kixero/blockname/BlockName.java
@@ -20,9 +20,12 @@ import org.jetbrains.annotations.NotNull;
 
 public final class BlockName extends JavaPlugin implements Listener
 {
-    private static final int DISTANCE_TO_BLOCK = 5;
-    private static boolean BOLD_TEXT = true;
+    private static final int DISTANCE_TO_BLOCK = 5;  // The max distance to the nearest block
+    private static boolean BOLD_TEXT = true;         // Display the text in bold
     
+    /**
+     * OnEnable() method.
+     */
     @Override
     public void onEnable()
     {
@@ -30,28 +33,133 @@ public final class BlockName extends JavaPlugin implements Listener
         
         getServer().getPluginManager().registerEvents(this, this);
         
+        // Load the Settings
         BOLD_TEXT = this.getConfig().getBoolean("settings.bold-text");
 
+        // Reload the names
         ConfigurationSection names = this.getConfig().getConfigurationSection("names");
     
         for (String id : names.getKeys(false))
         {
             ConfigurationSection section = this.getConfig().getConfigurationSection("names." + id);
-    
-            String name = section.getString("name");
-            String world = section.getString("world");
-            int x = section.getInt("x");
-            int y = section.getInt("y");
-            int z = section.getInt("z");
-    
-            Location loc = new Location(this.getServer().getWorld(world), x, y, z);
-    
-            Block block = loc.getBlock();
-    
-            this.addBlockName(block, name);
+            
+            Location loc = new Location(this.getServer().getWorld(section.getString("world")),
+                                        section.getInt("x"),
+                                        section.getInt("y"),
+                                        section.getInt("z"));
+            
+            this.addBlockName(loc.getBlock(), section.getString("name"));
         }
     }
     
+    /**
+     * OnCommand() Listener
+     *
+     * Listens to the command 'blockname' to set a name to a block. and to
+     * the command 'blockname remove' to remove a block's name.
+     *
+     * @param sender The sender of the command.
+     * @param cmd The command sent.
+     * @param label The label of the command.
+     * @param args The arguments of the command.
+     * @return True if the command has been handled, else otherwise.
+     */
+    public boolean onCommand(CommandSender sender, @NotNull Command cmd, String label, String[] args)
+    {
+        // Checks for the 'blockname' command.
+        if (cmd.getName().equalsIgnoreCase("blockname"))
+        {
+            // Check if the sender is a player
+            if (!(sender instanceof Player))
+            {
+                sender.sendMessage("This command can only be run by a player.");
+            }
+            else
+            {
+                Player player = (Player) sender;
+    
+                Block block = player.getTargetBlock(null, DISTANCE_TO_BLOCK);
+                
+                // Check if the player really aims at a block
+                if (block.getType() == Material.AIR)
+                {
+                    sender.sendMessage(ChatColor.GOLD + "You are not targeting a block.");
+                }
+                else
+                {
+                    // Check for the 'blockname remove' command
+                    if (args[0].equalsIgnoreCase("remove"))
+                    {
+                        this.removeBlockName(player, block);
+                    }
+                    else
+                    {
+                        // Builds the name from the multiple arguments (separated with a space)
+                        String blockName = "";
+        
+                        for (String str : args)
+                        {
+                            blockName = blockName.concat(str);
+                            blockName = blockName.concat(" ");
+                        }
+        
+                        blockName = blockName.substring(0, blockName.length() - 1);
+        
+                        // Add the name to the block
+                        this.addBlockName(player, block, blockName);
+                    }
+                }
+            
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * A PlayerMoveEvent Listener to check if the player looks at a named block
+     *
+     * @param event The playerMoveEvent caught.
+     * @return Always false.
+     */
+    @EventHandler
+    public boolean onPlayerMoveEvent(@NotNull PlayerMoveEvent event)
+    {
+        Player player = event.getPlayer();
+        Block block = player.getTargetBlock(null, DISTANCE_TO_BLOCK);
+        
+        if (block.hasMetadata("Name"))
+        {
+            sendMessage(player, block.getMetadata("Name").get(0).asString());
+        }
+        
+        return false;
+    }
+    
+    /**
+     * A BlockBreak Listener to check if a named block is destroyed
+     * and remove its name.
+     *
+     * @param event The BlockBreakEvent caught.
+     * @return Always false.
+     */
+    @EventHandler
+    public boolean onBlockDestroy(@NotNull BlockBreakEvent event)
+    {
+        if (event.getBlock().hasMetadata("Name"))
+        {
+            this.removeBlockName(event.getBlock());
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Displays the message in the player's actionbar.
+     *
+     * @param player The player to display the message.
+     * @param message The message to display as a String.
+     */
     private static void sendMessage(@NotNull Player player, String message)
     {
         if (BOLD_TEXT)
@@ -66,52 +174,11 @@ public final class BlockName extends JavaPlugin implements Listener
         }
     }
     
-    public boolean onCommand(CommandSender sender, @NotNull Command cmd, String label, String[] args)
-    {
-        if (cmd.getName().equalsIgnoreCase("blockname"))
-        {
-            if (!(sender instanceof Player))
-            {
-                sender.sendMessage("This command can only be run by a player.");
-            }
-            else
-            {
-                Player player = (Player) sender;
-    
-                Block block = player.getTargetBlock(null, DISTANCE_TO_BLOCK);
-                
-                if (block.getType() == Material.AIR)
-                {
-                    sender.sendMessage(ChatColor.GOLD + "You are not targeting a block.");
-                }
-                else
-                {
-                    if (args[0].equalsIgnoreCase("remove"))
-                    {
-                        this.removeBlockName(player, block);
-                    }
-                    else
-                    {
-                        String blockName = "";
-        
-                        for (String str : args)
-                        {
-                            blockName = blockName.concat(str);
-                            blockName = blockName.concat(" ");
-                        }
-        
-                        blockName = blockName.substring(0, blockName.length() - 1);
-        
-                        this.addBlockName(player, block, blockName);
-                    }
-                }
-            
-                return true;
-            }
-        }
-        return false;
-    }
-    
+    /**
+     * A method to remove a block's name in the metadata and the config.
+     *
+     * @param block The block we want to remove the name.
+     */
     private void removeBlockName(@NotNull Block block)
     {
         if (block.hasMetadata("Name"))
@@ -122,6 +189,13 @@ public final class BlockName extends JavaPlugin implements Listener
         this.saveConfig();
     }
     
+    /**
+     * A method to remove a block's name in the metadata and the config,
+     * and send a message to the player.
+     *
+     * @param player The player to send the message.
+     * @param block The block we want to remove the name.
+     */
     private void removeBlockName(Player player, @NotNull Block block)
     {
         if (block.hasMetadata("Name"))
@@ -132,6 +206,13 @@ public final class BlockName extends JavaPlugin implements Listener
         }
     }
     
+    /**
+     * A method to add (or change if name already set) a block's name
+     * in the metadata and in the config.
+     *
+     * @param block The block we want to send the message.
+     * @param blockName The name to set to the block.
+     */
     private void addBlockName(Block block, String blockName)
     {
         this.removeBlockName(block);
@@ -150,6 +231,14 @@ public final class BlockName extends JavaPlugin implements Listener
         this.saveConfig();
     }
     
+    /**
+     * A method to add (or change if name already set) a block's name
+     * in the metadata and in the config and send a message to the player.
+     *
+     * @param player The player to send the message.
+     * @param block The block we want to send the message.
+     * @param blockName The name to set to the block.
+     */
     private void addBlockName(@NotNull Player player, Block block, String blockName)
     {
         this.addBlockName(block, blockName);
@@ -157,6 +246,12 @@ public final class BlockName extends JavaPlugin implements Listener
         player.sendMessage(ChatColor.GOLD + "Name set.");
     }
     
+    /**
+     * A method to create a unique id based on a location coordinates.
+     *
+     * @param loc The location to base the id on.
+     * @return The id as a String.
+     */
     @NotNull
     private static String locToId(@NotNull Location loc)
     {
@@ -164,6 +259,8 @@ public final class BlockName extends JavaPlugin implements Listener
     }
     
     /**
+     * Method copied and tweaked from the ChatColor class to set all text in bold.
+     *
      * Translates a string using an alternate color code character into a
      * string that uses the internal ChatColor.COLOR_CODE color code
      * character. The alternate color code character will only be replaced if
@@ -171,9 +268,9 @@ public final class BlockName extends JavaPlugin implements Listener
      *
      * @param altColorChar The alternate color code character to replace. Ex: &
      * @param textToTranslate Text containing the alternate color code character.
-     * @return Text containing the ChatColor.COLOR_CODE color code character.
+     * @return Text containing the ChatColor.COLOR_CODE color code character and set to bold.
      */
-    private static String translateAlternateColorCodesBold(char altColorChar, String textToTranslate)
+    private static String translateAlternateColorCodesBold(char altColorChar, @NotNull String textToTranslate)
     {
         char[] b = textToTranslate.toCharArray();
         
@@ -194,46 +291,33 @@ public final class BlockName extends JavaPlugin implements Listener
         return new String(b);
     }
     
-    private static char[] insert(char[] a, int index, char c)
+    /**
+     * A method to insert a char in a char array at an index and shift
+     * the other elements after the index.
+     *
+     * @param array The array to insert the char into.
+     * @param index The index to insert the char at.
+     * @param c The char to insert.
+     * @return The array with the char inserted.
+     */
+    private static char[] insert(@NotNull char[] array, int index, char c)
     {
-        char[] result = new char[a.length + 1];
+        char[] result = new char[array.length + 1];
         
         for(int i = 0; i < index; i++)
-            result[i] = a[i];
+            result[i] = array[i];
         
         result[index] = c;
         
-        for(int i = index + 1; i < a.length + 1; i++)
-            result[i] = a[i - 1];
+        for(int i = index + 1; i < array.length + 1; i++)
+            result[i] = array[i - 1];
         
         return result;
     }
     
-    @EventHandler
-    public boolean onPlayerMoveEvent(@NotNull PlayerMoveEvent event)
-    {
-        Player player = event.getPlayer();
-        Block block = player.getTargetBlock(null, DISTANCE_TO_BLOCK);
-        
-        if (block.hasMetadata("Name"))
-        {
-            sendMessage(player, block.getMetadata("Name").get(0).asString());
-        }
-        
-        return false;
-    }
-    
-    @EventHandler
-    public boolean onBlockDestroy(@NotNull BlockBreakEvent event)
-    {
-        if (event.getBlock().hasMetadata("Name"))
-        {
-            this.removeBlockName(event.getBlock());
-        }
-        
-        return false;
-    }
-    
+    /**
+     * OnDisable() method.
+     */
     @Override
     public void onDisable()
     {
